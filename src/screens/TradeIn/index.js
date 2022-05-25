2f49c7347fe81a85a6ed678ac2843cb0acbe40f5
Noman Ali <nomanali7296@gmail.com>
2022-05-25 23:29:35 +0500

    trade

---
+++ src/screens/TradeIn/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View,ScrollView,Text,Heading } from "native-base";
+
+export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View >
+      </View>
+    );
+  }
+}
