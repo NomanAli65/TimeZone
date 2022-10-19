4a2ad51acc1e6bfb60f71214dc92645a06ef36e8
nomanali65 <nomanali7296@gmail.com>
2022-10-19 20:22:19 +0500

    done

--- src/screens/Auth/SocialSignin.js
+++ src/screens/Auth/SocialSignin.js
@@ -7,7 +7,7 @@ import { AntDesign, Ionicons, FontAwesome5, Fontisto } from "@expo/vector-icons"
 import { ResponseType } from 'expo-auth-session';
 import { post } from '../../configs/AxiosConfig';
 import axios from 'axios';
-import { Settings, LoginManager, Profile } from 'react-native-fbsdk-next';
+// import { Settings, LoginManager, Profile } from 'react-native-fbsdk-next';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -19,42 +19,45 @@ function SocialSignin(props) {
         androidClientId: '230281440299-gh9fhva6sopi9nn9i8dv23h4vbeafpjr.apps.googleusercontent.com',
         webClientId: '230281440299-n913skplf8in3pb0lnsou2vc9spt0pou.apps.googleusercontent.com',
     });
+    // const [requestFB, responseFB, promptAsyncFB] = Facebook.useAuthRequest({
+    //     client
+    // });
 
-    useEffect(() => {
-        Settings.setAppID('495885272460928');
-        Settings.initializeSDK();
-    }, [])
+    // useEffect(() => {
+    //     Settings.setAppID('495885272460928');
+    //     Settings.initializeSDK();
+    // }, [])
 
     const _GoogleSignin = async () => {
-        let result = await promptAsync();
-        if (result.authentication?.accessToken) {
-            let userData = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-                headers: {
-                    Authorization: "Bearer " + result.authentication?.accessToken
-                }
-            })
-            let { name, picture, email, } = userData?.data;
-            alert(name + "\n" + picture + "\n" + email,)
-        }
-        else if (result.error)
-            alert("Error occured. Please try again")
+        // let result = await promptAsync();
+        // if (result.authentication?.accessToken) {
+        //     let userData = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        //         headers: {
+        //             Authorization: "Bearer " + result.authentication?.accessToken
+        //         }
+        //     })
+        //     let { name, picture, email, } = userData?.data;
+        //     alert(name + "\n" + picture + "\n" + email,)
+        // }
+        // else if (result.error)
+        //     alert("Error occured. Please try again")
     }
 
     const _FacebookSignin = async () => {
-        LoginManager.logInWithPermissions(["public_profile"]).then(
-            function (result) {
-                if (result.isCancelled) {
-                    console.log("Login cancelled");
-                } else {
-                    Profile.getCurrentProfile().then((prof) => {
-                        alert(prof.email + "\n" + prof.name + "\n" + prof.imageURL);
-                    })
-                }
-            },
-            function (error) {
-                console.log("Login fail with error: " + error);
-            }
-        );
+        // LoginManager.logInWithPermissions(["public_profile"]).then(
+        //     function (result) {
+        //         if (result.isCancelled) {
+        //             console.log("Login cancelled");
+        //         } else {
+        //             Profile.getCurrentProfile().then((prof) => {
+        //                 alert(prof.email + "\n" + prof.name + "\n" + prof.imageURL);
+        //             })
+        //         }
+        //     },
+        //     function (error) {
+        //         console.log("Login fail with error: " + error);
+        //     }
+        // );
     }
 
     return (
