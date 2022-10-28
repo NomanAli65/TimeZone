d541585a5a0282cd49692549bf076ef8b5865682
nomanali65 <nomanali7296@gmail.com>
2022-10-28 19:09:54 +0500

    done build ios

--- src/screens/Auth/SocialSignin.js
+++ src/screens/Auth/SocialSignin.js
@@ -95,7 +95,7 @@ function SocialSignin(props) {
             });
             if (result.params?.access_token) {
                 setFBLoading(true)
-                let userData = await axios.get("https://graph.facebook.com/v15.0/me?fields=email,name&access_token=EAAHDAUmDroABAGT3pdTdXaeZAzPuPn8O7XaIBjGF6fvRQRkAs2wKkfBjTjGfe73Pzf3KaByOkEsTBU3L0d4wZAjLD2WSli0wsXsfACjihSczA5ZCp2ALBueraYkfWALRhoOknHAHsJawSCvUm1JgHB0LpKX676oZCl8FbWhrfxfZAdJTChdzt0QtTmVNZBWVBkoLxISgezTrYKF9GaC1ZBS")// + result.params?.access_token)
+                let userData = await axios.get("https://graph.facebook.com/v15.0/me?fields=email,name&access_token=" + result.params?.access_token)
                 let { name, email, id } = userData?.data;
                 if (!email) {
                     dispatch(AlertAction.ShowAlert({
