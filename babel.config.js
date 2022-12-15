e4b15ab5e18989c774028efe7e1ebdbd3a9d09ca
nomanali65 <nomanali7296@gmail.com>
2022-12-15 15:05:29 +0500

    done

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', "module:metro-react-native-babel-preset"],
+    presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-    //   ["module-resolver",
-    //     {
-    //       root: ["."],
-    //       resolvePath(sourcePath, currentFile) {
-    //         if (
-    //           sourcePath === "react-native" &&
-    //           !(
-    //             (
-    //               currentFile.includes("node_modules/react-native/") || // macos/linux paths
-    //               currentFile.includes("node_modules\\react-native\\")
-    //             ) // windows path
-    //           ) &&
-    //           !(
-    //             currentFile.includes("resolver/react-native/") ||
-    //             currentFile.includes("resolver\\react-native\\")
-    //           )
-    //         ) {
-    //           return path.resolve(__dirname, "resolver/react-native");
-    //         }
-    //         return undefined;
-    //       },
-    //     },
-    //   ]
      ],
   };
 };
