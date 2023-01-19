969ffb86b84e1bdf92995e892f27513c36a7f2eb
nomanali65 <nomanali7296@gmail.com>
2023-01-19 21:10:57 +0500

    done notifications with iamge on both platforms

---
+++ ios/ImageNotifications/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotifications
+//
+//  Created by Noman Ali on 1/19/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
